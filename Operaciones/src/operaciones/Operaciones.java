b70e119c32ba16a5e7cc653bf97192cb9f238730
BranditonXD200 <jbsema@misena.edu.co>
2016-01-31 17:26:02 -0600

    Division

--- Operaciones/src/operaciones/Operaciones.java
+++ Operaciones/src/operaciones/Operaciones.java
@@ -15,8 +15,8 @@ public class Operaciones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a = 5, b = 10, c = a - b;
-        System.out.println(a + " - " + b + " = " + c);
+        int a = 5, b = 10, c = a / b;
+        System.out.println(a + " / " + b + " = " + c);
     }
 
 }
